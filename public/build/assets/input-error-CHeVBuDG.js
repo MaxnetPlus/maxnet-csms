e7de18b374f311eeb4e31391669aaccd98e7daf8
Muhammad Nur Ikhsanuddin <108898180+nurikhsanuddin@users.noreply.github.com>
2025-07-24 11:45:17 +0700

    asset build

--- public/build/assets/input-error-CHeVBuDG.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-DOxJ3Wnc.js";import{c as o}from"./createLucideIcon-DZIc7J5K.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
